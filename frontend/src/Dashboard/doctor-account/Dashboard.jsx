c2b2c60415776c636346e1f1c689e5faea3dd663
rajon38 <45762739+rajon38@users.noreply.github.com>
2024-12-08 23:47:21 +0600

    12-8-24 frontend update

--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import Loader from '../../components/Loader/Loading'
 import Error from '../../components/Error/Error'
 import  userGetProfile  from '../../hooks/useFetchData'
 import { BASE_URL } from '../../config'
 import Tabs from './Tabs'
 import starIcon from '../../assets/images/Star.png'
+import DoctorAbout from './../../pages/Doctors/DoctorAbout';
+import Profile from './Profile';
+import Appointments from './Appointments'
 
 const Dashboard = () => {
 
@@ -58,26 +61,32 @@ const Dashboard = () => {
 
                     <div>
                       <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold">
-                        Surgeon
+                        {data.specialization}
                       </span>
 
-                      <h3 className="text-[22px] leading-9 font-bold text-headingColor mt-3">Rashedul Islam</h3>
+                      <h3 className="text-[22px] leading-9 font-bold text-headingColor mt-3">{data.name}</h3>
 
                       <div className="flex items-center gap-[6px]">
                         <span className="flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
-                          <img src={starIcon} alt="" />4.5
+                          <img src={starIcon} alt="" />{data.averageRating}
                         </span>
                         <span className=" text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold">
-                          (22)
+                          ({data.totalRating})
                         </span>
                       </div>
 
-                      <p className="text_para font-[15px] lg:max-w-[390px] leading-6">Doctor Bio</p>
+                      <p className="text_para font-[15px] lg:max-w-[390px] leading-6">{data?.bio}</p>
                     </div>
                   </div>
+                  <DoctorAbout
+                      name={data.name}
+                      about={data.about}
+                      qualifications={data.qualifications}
+                      experiences={data.experiences}
+                    />
                 </div>}
-                {tab == 'appointments' && <div>appointments</div>}
-                {tab == 'settings' && <div>settings</div>}
+                {tab == 'appointments' && <Appointments appointments= {data.appointments}/>}
+                {tab == 'settings' && <Profile doctorData={data}/>}
               </div>
               </div>
             </div>
--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
 import { formateDate } from '../../utils/formateDate'
 
-const DoctorAbout = () => {
+const DoctorAbout = ({name, about, qualifications, experiences}) => {
   return (
     <div>
         <div>
             <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
                 About of
                 <span className="text-irisBlueColor font-bold text-[24px] leading-9">
-                    Rashedul Alom
+                    {name}
                 </span>
             </h3>
-            <p className="text_para">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, reprehenderit omnis. Ab in magnam obcaecati minus, dicta vitae, ullam velit unde aperiam eos molestias consectetur quaerat, distinctio dolor consequatur porro.</p>
+            <p className="text_para">{about}</p>
         </div>
 
         <div className="mt-12">
@@ -20,24 +20,19 @@ const DoctorAbout = () => {
             </h3>
 
             <ul className="pt-4 md:p-5">
-                <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
-                    <div>
-                        <span className="text-irisBlueColor text-[15px] leading-6 font-semibold text-textColor">
-                        {formateDate("07-07-2017")} - {formateDate("07-07-2019")}
-                        </span>
-                        <p className="text-[16px] leading-6 font-medium text-textColor">PHD in Surgeon</p>
-                    </div>
-                    <p className="text-[14px] leading-6 font-medium text-textColor">New Apollo Hospital, New York</p>
-                </li>
-                <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
-                    <div>
-                        <span className="text-irisBlueColor text-[15px] leading-6 font-semibold text-textColor">
-                            {formateDate("10-05-2011")} - {formateDate("07-07-2015")}
-                        </span>
-                        <p className="text-[16px] leading-6 font-medium text-textColor">PHD in Surgeon</p>
-                    </div>
-                    <p className="text-[14px] leading-6 font-medium text-textColor">New Apollo Hospital, New York</p>
-                </li>
+
+                {qualifications?.map((item, index)=>
+                <li key={index} className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+                <div>
+                    <span className="text-irisBlueColor text-[15px] leading-6 font-semibold text-textColor">
+                    {formateDate(item.startingDate)} - {formateDate(item.endingDate)}
+                    </span>
+                    <p className="text-[16px] leading-6 font-medium text-textColor">{item.degree}</p>
+                </div>
+                <p className="text-[14px] leading-6 font-medium text-textColor">{item.university}</p>
+            </li>)}
+                
+                
             </ul>
         </div>
 
@@ -47,20 +42,15 @@ const DoctorAbout = () => {
             </h3>
 
             <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
-                <li className="p-4 rounded bg-[#fff9ea]">
-                    <span className="text-yellowColor text-[15px] leading-6 font-semibold">
-                    {formateDate("07-07-2010")} - {formateDate("07-07-2019")}
-                    </span>
-                    <p className="text-[16px] leading-6 font-medium text-textColor">Sr. Surgeon</p>
-                    <p className="text-[14px] leading-5 font-medium text-textColor">New Apollo Hospital, New York</p>
-                </li>
-                <li className="p-4 rounded bg-[#fff9ea]">
-                    <span className="text-yellowColor text-[15px] leading-6 font-semibold">
-                    {formateDate("07-07-2010")} - {formateDate("07-07-2019")}
-                    </span>
-                    <p className="text-[16px] leading-6 font-medium text-textColor">Sr. Surgeon</p>
-                    <p className="text-[14px] leading-5 font-medium text-textColor">New Apollo Hospital, New York</p>
-                </li>
+                {experiences?.map((item,index)=>
+                <li key={index} className="p-4 rounded bg-[#fff9ea]">
+                <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+                {formateDate(item.startingDate)} - {formateDate(item.endingDate)}
+                </span>
+                <p className="text-[16px] leading-6 font-medium text-textColor">{item.position}</p>
+                <p className="text-[14px] leading-5 font-medium text-textColor">{item.hospital}</p>
+            </li>)}
+                
             </ul>
         </div>
     </div>
