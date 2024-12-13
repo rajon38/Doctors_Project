5d9b0f9201fe641975a6bf31a915c4d687438f00
rajon38 <45762739+rajon38@users.noreply.github.com>
2024-12-13 23:54:30 +0600

    Doctor profile review part complete

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs';
 
 const DoctorCard = ({doctor}) => {
-    const {name, specialization, avgRating, totalRating, photo, totalPatients, hospital} = doctor
+    const {name, specialization, avgRating, totalRating, photo, experiences} = doctor
   return (
     <div className='p-3 lg:p-5'>
         <div>
@@ -33,15 +33,15 @@ const DoctorCard = ({doctor}) => {
 
         <div className="mt-[18px] lg:mt-5 flex items-center justify-between">
             <div>
-                <h3 className="text-[16px] leading-7 lg:text-[18px] lg:leading-[30px] font-semibold text-headingColor">
+                {/* <h3 className="text-[16px] leading-7 lg:text-[18px] lg:leading-[30px] font-semibold text-headingColor">
                     +{totalPatients} patients
-                </h3>
+                </h3> */}
                 <p className="text-[14px] leading-6 font-[400] text-textColor">
-                    At {hospital}
+                    At {experiences && experiences[0]?.hospital}
                 </p>
             </div>
 
-            <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
+            <Link to={`/doctors/${doctor._id}`} className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
                 <BsArrowRight className='group-hover:text-white w-6 h-5'/>
             </Link>
         </div>
